885c4f154d0fb07226003a8a7044fbb5252f7c22
Santiago <santiago.gonzalezagudelo@gmail.com>
2022-12-02 13:57:50 -0500

    Primer Commit :D

---
+++ tallerCondi.js
@@ -0,0 +1,95 @@
+//-------------------------- 1
+
+// let num1=+prompt('Digite un número')
+// let num2=+prompt('Digite otro número')
+
+// if (num1>num2){
+//     alert(`${num2},${num1}`)
+
+// }else{
+//     alert(`${num1},${num2}`)
+// }
+
+//--------------------------- 2
+
+// let num1=+prompt('Digite un número')
+
+// if (num1>=1 && num1<=10){
+//     if (num1%2==0){
+//         alert('Es un número par')
+//     }else{
+//         alert('Es un número impar')
+//     }
+// }else{
+//     alert('Número fuera del rango')
+// }
+
+//----------------------------- 3
+
+// let num1=prompt('Ingrese un número')
+
+// if (num1>=1 && num1<=10){
+//     if (num1==1){
+//         alert('1 = Uno')
+//     }else if (num1==2){
+//         alert('2 = Dos')
+//     }else if (num1==3){
+//         alert('3 = Tres')
+//     }else if (num1==4){
+//         alert('4 = Cuatro')
+//     }else if (num1==5){
+//         alert('5 = Cinco')
+//     }else if (num1==6){
+//         alert('6 = Seis')
+//     }else if (num1==7){
+//         alert('7 = Siete')
+//     }else if (num1==8){
+//         alert('8 = Ocho')
+//     }else if (num1==9){
+//         alert('9 = Nueve')
+//     }else if (num1==10){
+//         alert('10 = Diez')
+//     }
+// }else{
+//     alert('Número fuera del rango')
+// }
+
+//------------------------------- 4
+
+// let minutos=+prompt('Ingrese número de minutos usados')
+
+// if (minutos<=3){
+//     alert('Su llamada tiene un costo de $200')
+
+// }else{
+//     minutos-=3
+//     alert(`Su llamada tiene un costo de ${200+(minutos*100)}`)
+// }
+
+//---------------------------------- 5
+
+// let precioConejosB=+prompt('Ingrese precio del conejo blanco')
+// let ConejosB=+prompt('Ingrese cantidad de conejos blancos vendida')
+// let precioConejosN=+prompt('Ingrese precio del conejo negro')
+// let ConejosN=+prompt('Ingrese cantidad de conejos negros vendida')
+
+// cantidadVendi=(ConejosB+ConejosN)
+
+// p1=(ConejosB*precioConejosB)
+// p2=(ConejosN*precioConejosN)
+
+// montoTo=p1+p2
+// mayor=0
+// if (ConejosB>ConejosN){
+//     mayor='Blancos'
+// }else{
+//     mayor='Negros'
+// }
+// alert(`
+// Cantidad de conejos vendida: ${cantidadVendi}
+// Monto Total de venta: ${montoTo}
+// Mayor número de conejos vendidos: ${mayor}`)
+
+//----------------------------------- 6 
+
+
